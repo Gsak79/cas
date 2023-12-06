c0ea3303fb70cfc160c9c597d88a0ac8fd33feff
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-12-06 00:22:07 -0500

    Add entry fields in model

--- models/Entry.js
+++ models/Entry.js
@@ -2,9 +2,25 @@ import mongoose from "mongoose";
 
 const entrySchema = new mongoose.Schema({
     title: String,
-    description: String,
-    body: String,
-    date: Date,
+    description1: String,
+    description2: String,
+    description3: String,
+    reflexion1: String,
+    reflexion2: String,
+    reflexion3: String,
+    resultado1: String,
+    resultado2: String,
+    resultado3: String,
+    date1: Date,
+    date2: Date,
+    date3: Date,
+    meta: String,
+    permisos: String,
+    pasos: String,
+    dificultades: String,
+    riesgos: String,
+    experience: String,
+
     images: [
         {
             _id: false,
